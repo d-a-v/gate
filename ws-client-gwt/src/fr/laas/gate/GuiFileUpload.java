ba982501831378e13adc83d46e34201bcee11703
Adrien Thibaud <athibaud@laas.fr>
2013-07-24 10:15:01 +0200

    GuiFileUpload avec FormPanel (c'est nécéssaire d'après la doc mais ça ne marche pas)

--- ws-client-gwt/src/fr/laas/gate/GuiFileUpload.java
+++ ws-client-gwt/src/fr/laas/gate/GuiFileUpload.java
@@ -65,6 +65,7 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Widget;
 
 
@@ -87,48 +88,87 @@ class GuiFileUpload extends FormPanel implements IntfObject
 	
 	public GuiFileUpload (IntfObject parent, String name)
 	{
-		//super();
+		super();
 		setStyleName("");
 		
 		this.name = name;
 		this.parent = parent;
 		sons = new ArrayList<IntfObject>();
-		VerticalPanel v = new VerticalPanel();
-		this.add(v);
-		HorizontalPanel h1 = new HorizontalPanel();
+		// Create a FormPanel and point it at a service.
+		setAction("/myFormHandler");
+
+		// Because we're going to add a FileUpload widget, we'll need to set the
+		// form to use the POST method, and multipart MIME encoding.
+		setEncoding(FormPanel.ENCODING_MULTIPART);
+		setMethod(FormPanel.METHOD_POST);
+
+		// Create a panel to hold all of the form widgets.
+		VerticalPanel panel = new VerticalPanel();
+		setWidget(panel);
+
+		// Create a TextBox, giving it a name so that it will be submitted.
+		final TextBox tb = new TextBox();
+		tb.setName("textBoxFormElement");
+		add(tb);
+
+		// Create a ListBox, giving it a name and some values to be associated with
+		// its options.
+		ListBox lb = new ListBox();
+		lb.setName("listBoxFormElement");
+		lb.addItem("foo", "fooValue");
+		lb.addItem("bar", "barValue");
+		lb.addItem("baz", "bazValue");
+		add(lb);
+
+		// Create a FileUpload widget.
+		FileUpload upload = new FileUpload();
+		upload.setName("uploadFormElement");
+		panel.add(upload);
+
+		// Add a 'submit' button.
+		panel.add(new Button("Submit", new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				submit();
+			}
+		}));
+
+		// Add an event handler to the form.
+		addSubmitHandler(new FormPanel.SubmitHandler() {
+			public void onSubmit(SubmitEvent event) {
+			// This event is fired just before the form is submitted. We can take
+			// this opportunity to perform validation.
+				if (tb.getText().length() == 0) {
+					Window.alert("The text box must not be empty");
+					event.cancel();
+				}
+			}
+		});
+		addSubmitCompleteHandler(new FormPanel.SubmitCompleteHandler() {
+			public void onSubmitComplete(SubmitCompleteEvent event) {
+			// When the form submission is successfully completed, this event is
+			// fired. Assuming the service returned a response of type text/html,
+			// we can get the result text here (see the FormPanel documentation for
+			// further explanation).
+				Window.alert(event.getResults());
+			}
+		});
+		
+		/*HorizontalPanel h1 = new HorizontalPanel();
 		HorizontalPanel h2 = new HorizontalPanel();
 		final TextBox t = new TextBox();
 		t.setName(this.name + "Text");
-		FileUpload f = new FileUpload();
+		final FileUpload f = new FileUpload();
 		h1.add(t);
 		h1.add(f);
 		t.setText(f.getFilename());
 		h2.add(new Button("Submit", new ClickHandler() {
 			public void onClick(ClickEvent event) {
-			submit();
+			t.setText(f.getFilename());
+			//submit();
 			}
 		}));
-		/*addSubmitHandler(new FormPanel.SubmitHandler() {
-			public void onSubmit(SubmitEvent event) {
-	        // This event is fired just before the form is submitted. We can take
-   		    // this opportunity to perform validation.
-				if (t.getText().length() == 0) {
-			        Window.alert("The text box must not be empty");
-       				event.cancel();
-        		}
-      		}
-    	});
-		addSubmitCompleteHandler(new FormPanel.SubmitCompleteHandler() {
-		    public void onSubmitComplete(SubmitCompleteEvent event) {
-    	    // When the form submission is successfully completed, this event is
-    	    // fired. Assuming the service returned a response of type text/html,
-    	    // we can get the result text here (see the FormPanel documentation for
-    	    // further explanation).
-    	    Window.alert(event.getResults());
-    		}
-    	});*/
-		v.add(h1);
-		v.add(h2);
+		this.add(h1);
+		this.add(h2);*/
 
 		place = new Place(this);
 		// we are container, gap will be useful by inside objects
